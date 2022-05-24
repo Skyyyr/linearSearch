8544cf08b726eb1787d51d7287766e3b7a1915b3
Skyyyr <emuswgluvr@gmail.com>
2022-05-24 14:52:43 -0700

    First

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,6 +1,6 @@
 const arrayToSearchThrough = [];
 for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
+    arrayToSearchThrough.push(i);//test
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
