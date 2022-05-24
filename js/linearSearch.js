98038c7164d69b75fd5ef1057b306d01acf19672
Skyyyr <emuswgluvr@gmail.com>
2022-05-24 16:12:39 -0700

    Finished both linear searches

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,21 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        const currentIndex = arrayToSearchThrough[i];
+        if (valueToFind === currentIndex) {
+            return i;
+        }
+    }
 };
 
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+    let arrayOfIndexes = new Array;
+    for (let i = 0; i < arrayToSearchThrough.length; i++) {
+        const currentIndex = arrayToSearchThrough[i];
+        if (valueToFind === currentIndex) {
+            arrayOfIndexes.push(i);
+        }
+    }
+    return arrayOfIndexes;
+}
