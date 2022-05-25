6fee862d46247c8300daef0d5c9b3d9fe3369c83
Skyyyr <emuswgluvr@gmail.com>
2022-05-25 12:54:44 -0700

    Python linear search

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,15 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+        for i in range(len(array_to_search_through)):
+            current_index = array_to_search_through[i]
+            if value_to_find == current_index:
+                return i
+
+def linear_search_global(value_to_find, array_to_search_through):
+    output_array = []
+    for i in range(len(array_to_search_through)):
+        current_index = array_to_search_through[i]
+        if value_to_find == current_index:
+            output_array.append(i)
+    return output_array
